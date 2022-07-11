9ed01e07a0ff61c33a51b98c9962663426f51801
FeNjK <bodhisatva_xp@mail.ru>
2022-07-11 12:07:11 +0300

    После первого ревью

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import Header from './Header.js';
-import Main from './Main';
-import Footer from './Footer';
-import ImagePopup from './ImagePopup';
-import PopupWithForm from './PopupWithForm';
+import React from "react";
+import Header from "./Header.js";
+import Main from "./Main";
+import Footer from "./Footer";
+import ImagePopup from "./ImagePopup";
+import PopupWithForm from "./PopupWithForm";
 
 function App() {
-
-  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
+    React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
-  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] =
+    React.useState(false);
   const [selectedCard, setSelectedCard] = React.useState(null);
 
   function handleEditAvatarClick() {
@@ -34,116 +35,128 @@ function App() {
     setIsAddPlacePopupOpen(false);
     setSelectedCard(null);
   }
-  
+
   return (
     <>
-    <Header />
-    <Main
-      onEditProfile={handleEditProfileClick}
-      onAddPlace={handleAddPlaceClick}
-      onEditAvatar={handleEditAvatarClick}
-      onCardClick={handleCardClick}
-    />
-    <Footer />
-    <ImagePopup 
-        card={selectedCard}
+      <Header />
+      <Main
+        onEditProfile={handleEditProfileClick}
+        onAddPlace={handleAddPlaceClick}
+        onEditAvatar={handleEditAvatarClick}
+        onCardClick={handleCardClick}
+      />
+      <Footer />
+      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+      <PopupWithForm
         onClose={closeAllPopups}
-    />
-    <PopupWithForm onClose={closeAllPopups}
-                   onEditProfile={handleEditProfileClick}
-                   name='edit'
-                   title='Редактировать профиль'
-                   buttonText='Сохранить'
-                   isOpen={`${isEditProfilePopupOpen ? 'popup_activ' : ''}`}
-    >
-      <fieldset className="popup__input-place"
-                name="popup__input-place">
-        <input type="text"
-               name="profile_name"
-               placeholder="Введите ваше имя"
-               className="popup__input popup__input_content_name"
-               id="profile_name"
-               minLength="2"
-               maxLength="40"
-               required/>
-        <span className="popup__validation-message popup__validation-message_position_first"
-              id="profile_name-error">
-        </span>
-        <input type="text"
-               name="type_of_activity"
-               placeholder="Укажите род ваших занятий"
-               className="popup__input popup__input_content_activity-type"
-               id="type_of_activity"
-               minLength="2"
-               maxLength="200"
-               required/>
-        <span className="popup__validation-message popup__validation-message_position_second"
-              id="type_of_activity-error">
-        </span>
-      </fieldset>
-    </PopupWithForm>
+        onEditProfile={handleEditProfileClick}
+        name="edit"
+        title="Редактировать профиль"
+        buttonText="Сохранить"
+        isOpen={`${isEditProfilePopupOpen ? "popup_activ" : ""}`}
+      >
+        <fieldset className="popup__input-place" name="popup__input-place">
+          <input
+            type="text"
+            name="profile_name"
+            placeholder="Введите ваше имя"
+            className="popup__input popup__input_content_name"
+            id="profile_name"
+            minLength="2"
+            maxLength="40"
+            required
+          />
+          <span
+            className="popup__validation-message popup__validation-message_position_first"
+            id="profile_name-error"
+          ></span>
+          <input
+            type="text"
+            name="type_of_activity"
+            placeholder="Укажите род ваших занятий"
+            className="popup__input popup__input_content_activity-type"
+            id="type_of_activity"
+            minLength="2"
+            maxLength="200"
+            required
+          />
+          <span
+            className="popup__validation-message popup__validation-message_position_second"
+            id="type_of_activity-error"
+          ></span>
+        </fieldset>
+      </PopupWithForm>
 
-    <PopupWithForm onClose={closeAllPopups}
-                   onAddPlace={handleAddPlaceClick}
-                   name='add-new-card'
-                   title='Новое место'
-                   buttonText='Сохранить'
-                   isOpen={`${isAddPlacePopupOpen ? 'popup_activ' : ''}`}
-    >
-      <fieldset className="popup__input-place"
-                name="popup__input-place">
-        <input type="text"
-               name="card-title"
-               placeholder="Название"
-               className="popup__input popup__input_content_image-title"
-               id="card-title"
-               minLength="2"
-               maxLength="30"
-               required/>
-        <span className="popup__validation-message popup__validation-message_position_first"
-              id="card-title-error">
-        </span>
-        <input type="url"
-               name="picture-link"
-               placeholder="Ссылка на картинку"
-               className="popup__input popup__input_content_image-link"
-               id="picture-link"
-               required/>
-        <span className="popup__validation-message popup__validation-message_position_second"
-              id="picture-link-error">
-        </span>
-      </fieldset>
-    </PopupWithForm>
-    
-    <PopupWithForm onClose={closeAllPopups}
-                   onEditProfile={handleEditAvatarClick}
-                   name='avatar'
-                   title='Обновить аватар'
-                   buttonText='Сохранить'
-                   isOpen={`${isEditAvatarPopupOpen ? 'popup_activ' : ''}`}
-    >
-      <fieldset className="popup__input-place"
-                name="popup__input-place">
-        <input type="url"
-               name="profile-avatar"
-               placeholder="Введите адрес изображения"
-               className="popup__input popup__input_content_avatar"
-               id="profile-avatar"
-               required/>
-        <span className="popup__validation-message popup__validation-message_position_first"
-              id="profile-avatar-error">
-        </span>
-      </fieldset>
-    </PopupWithForm>
+      <PopupWithForm
+        onClose={closeAllPopups}
+        onAddPlace={handleAddPlaceClick}
+        name="add-new-card"
+        title="Новое место"
+        buttonText="Сохранить"
+        isOpen={`${isAddPlacePopupOpen ? "popup_activ" : ""}`}
+      >
+        <fieldset className="popup__input-place" name="popup__input-place">
+          <input
+            type="text"
+            name="card-title"
+            placeholder="Название"
+            className="popup__input popup__input_content_image-title"
+            id="card-title"
+            minLength="2"
+            maxLength="30"
+            required
+          />
+          <span
+            className="popup__validation-message popup__validation-message_position_first"
+            id="card-title-error"
+          ></span>
+          <input
+            type="url"
+            name="picture-link"
+            placeholder="Ссылка на картинку"
+            className="popup__input popup__input_content_image-link"
+            id="picture-link"
+            required
+          />
+          <span
+            className="popup__validation-message popup__validation-message_position_second"
+            id="picture-link-error"
+          ></span>
+        </fieldset>
+      </PopupWithForm>
 
-    {/* Реализация функций удаления карточек и лайка будет осуществлена в 11 спринте */}
-    {/* <PopupWithForm title='Вы уверены?'
+      <PopupWithForm
+        onClose={closeAllPopups}
+        onEditProfile={handleEditAvatarClick}
+        name="avatar"
+        title="Обновить аватар"
+        buttonText="Сохранить"
+        isOpen={`${isEditAvatarPopupOpen ? "popup_activ" : ""}`}
+      >
+        <fieldset className="popup__input-place" name="popup__input-place">
+          <input
+            type="url"
+            name="profile-avatar"
+            placeholder="Введите адрес изображения"
+            className="popup__input popup__input_content_avatar"
+            id="profile-avatar"
+            required
+          />
+          <span
+            className="popup__validation-message popup__validation-message_position_first"
+            id="profile-avatar-error"
+          ></span>
+        </fieldset>
+      </PopupWithForm>
+
+      {/* Реализация функций удаления карточек и лайка будет осуществлена в 11 спринте */}
+      {/* <PopupWithForm title='Вы уверены?'
                    name='confirmation'
                    buttonText='Да'
                    ...
     >
     </PopupWithForm> */}
-  </>
+    </>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,83 +1,98 @@
-import React from 'react';
-import editingPen from '../images/editing-pen.svg';
-import buttonVector from '../images/button-vector.svg';
-import Card from './Card.js';
-import Api from '../utils/api.js';
+import { useEffect, useState } from "react";
+import editingPen from "../images/editing-pen.svg";
+import buttonVector from "../images/button-vector.svg";
+import Card from "./Card.js";
+import api from "../utils/api.js";
 
 function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick }) {
-  
-  const[userAvatar, setUserAvatar] = React.useState('');
-  const[userName, setUserName] = React.useState('');
-  const[userDescription, setUserDescription] = React.useState('');
-  const[cards, setCards] = React.useState([]);
+  const [userAvatar, setUserAvatar] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userDescription, setUserDescription] = useState("");
+  const [cards, setCards] = useState([]);
 
-  // Используем стейт для данных из API
+  // Используем стейт для данных из Api
   // (Хотя можно было все это перенести и в App.js...)
-  React.useEffect(() => {
-    // Запрос к Api за информацией о пользователе 
+  useEffect(() => {
+    // Запрос к Api за информацией о пользователе
     // и массиве карточек выполняется единожды, при монторивании
-    Api.getUserInfo()
+    api
+      .getUserInfo()
       .then((userData) => {
         setUserName(userData.name);
         setUserDescription(userData.about);
         setUserAvatar(userData.avatar);
       })
       .catch((err) => {
-        console.log(`Тут какая-то ошибка с получением пользовательских данных ${err}`)
-      })
+        console.log(
+          `Тут какая-то ошибка с получением пользовательских данных ${err}`
+        );
+      });
 
-    Api.getInitialCards()
+    api
+      .getInitialCards()
       .then((card) => {
         setCards(card);
       })
       .catch((err) => {
-        console.log(`Тут какая-то ошибка с получением массива карточек ${err}`)
-      })
-    }, [])
+        console.log(`Тут какая-то ошибка с получением массива карточек ${err}`);
+      });
+  }, []);
   // а вот если бы мы не поставили пустой массив последним
   // то вызоб совершался далеко не единожды
 
   return (
-    <>
     <main className="content">
-
       <section className="profile">
         <article className="profile-info">
-          <img className="profile-info__avatar"
-               src={userAvatar}
-               alt="Аватар пользователя"/>
-          <button className="profile-info__avatar-button"
-                  title="Редактирование аватара"
-                  onClick={onEditAvatar}
-                  aria-label="Кнопка редактирование аватара">
-            <img className="profile-info__avatar-pen"
-                 src={editingPen}
-                 alt="Перо редактирования аватара пользователя"/>
+          <img
+            className="profile-info__avatar"
+            src={userAvatar}
+            alt="Аватар пользователя"
+          />
+          <button
+            className="profile-info__avatar-button"
+            title="Редактирование аватара"
+            onClick={onEditAvatar}
+            aria-label="Кнопка редактирование аватара"
+          >
+            <img
+              className="profile-info__avatar-pen"
+              src={editingPen}
+              alt="Перо редактирования аватара пользователя"
+            />
           </button>
-          
+
           <div className="profile-info__data">
             <div className="profile-info__single-level">
               <h1 className="profile-info__name">{userName}</h1>
-              <button className="profile-info__editing-button"
-                   title="Редактирование профиля"
-                   onClick={onEditProfile}
-                   type="button"
-                   aria-label="Кнопка редактирование профиля">
-                <img className="profile-info__editing-pen"
-                     src={editingPen}
-                     alt="Перо редактирования имени и рода деятельности"/>
+              <button
+                className="profile-info__editing-button"
+                title="Редактирование профиля"
+                onClick={onEditProfile}
+                type="button"
+                aria-label="Кнопка редактирование профиля"
+              >
+                <img
+                  className="profile-info__editing-pen"
+                  src={editingPen}
+                  alt="Перо редактирования имени и рода деятельности"
+                />
               </button>
             </div>
             <p className="profile-info__activity-type">{userDescription}</p>
           </div>
         </article>
-        <button className="profile__add-button"
-                type="button"
-                title="Добавить фотографии"
-                onClick={onAddPlace}>
-          <img className="profile__button-vector"
-               src={buttonVector}
-               alt="Добавить фотографии"/>
+        <button
+          className="profile__add-button"
+          type="button"
+          title="Добавить фотографии"
+          onClick={onAddPlace}
+        >
+          <img
+            className="profile__button-vector"
+            src={buttonVector}
+            alt="Добавить фотографии"
+          />
         </button>
       </section>
 
@@ -87,7 +102,7 @@ function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick }) {
 
       <section className="photo-library">
         <ul className="elements">
-          { cards.map((card) => {
+          {cards.map((card) => {
             return (
               <Card
                 card={card}
@@ -96,15 +111,13 @@ function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick }) {
                 likes={card.likes}
                 key={card._id} // очень важное свойство!!!
                 onCardClick={onCardClick}
-              /> 
-            )}
-          )}
+              />
+            );
+          })}
         </ul>
       </section>
-
     </main>
-    </>
-  )
+  );
 }
 
 export default Main;
